745e168fe79ede4b32a21f18a2249b8d5ffb889f
Levin <1837307557@qq.com>
2018-03-03 11:19:18 +0800

    init

---
+++ chapter1/src/main/java/com/battcn/Application.java
@@ -0,0 +1,15 @@
+package com.battcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Levin
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
