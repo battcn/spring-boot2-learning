59acc4cc6cc93c29efb167a0ea02afcc20382055
Levin <1837307557@qq.com>
2018-04-12 09:44:41 +0800

    完成 AMQP 案例（1.手动ACK/2.延迟队列）

--- chapter2/src/main/java/com/battcn/controller/BookController.java
+++ chapter2/src/main/java/com/battcn/controller/BookController.java
@@ -40,7 +40,9 @@ public class BookController {
         // 添加延时队列
 
         this.rabbitTemplate.convertAndSend(RabbitConfig.REGISTER_DELAY_EXCHANGE, RabbitConfig.DELAY_ROUTING_KEY, book, message -> {
+            // TODO 第一句是可要可不要,根据自己需要自行处理
             message.getMessageProperties().setHeader(AbstractJavaTypeMapper.DEFAULT_CONTENT_CLASSID_FIELD_NAME, Book.class.getName());
+            // TODO 如果配置了 params.put("x-message-ttl", 5 * 1000); 那么这一句也可以省略,具体根据业务需要是声明 Queue 的时候就指定好延迟时间还是在发送自己控制时间
             message.getMessageProperties().setExpiration(5 * 1000 + "");
             return message;
         });
