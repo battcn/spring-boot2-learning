e488f5f1e03c67844d553965bcfdf90e3b7c6911
Levin <1837307557@qq.com>
2018-04-23 17:32:09 +0800

    第二篇：SpringBoot配置详解

--- chapter2/src/main/java/com/battcn/controller/PropertiesController.java
+++ chapter2/src/main/java/com/battcn/controller/PropertiesController.java
@@ -5,6 +5,7 @@ import com.battcn.properties.MyProperties2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,12 @@ public class PropertiesController {
     private static final Logger log = LoggerFactory.getLogger(PropertiesController.class);
     private final MyProperties1 myProperties1;
     private final MyProperties2 myProperties2;
+
+    /**
+     * TODO @Value 注解获取值方式,支持表达式，简单 eq and or 大于 小于 等对比操作
+     */
+    @Value("${my1.name}")
+    private String my1Name;
 
     @Autowired
     public PropertiesController(MyProperties1 myProperties1, MyProperties2 myProperties2) {
