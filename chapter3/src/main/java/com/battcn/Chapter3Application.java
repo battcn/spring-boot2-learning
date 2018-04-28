eef57c29c5a2f02a2b977348656d26ddee490ac5
Levin <1837307557@qq.com>
2018-04-28 14:56:27 +0800

    一起来学SpringBoot | 第四篇：整合Thymeleaf模板

---
+++ chapter3/src/main/java/com/battcn/Chapter3Application.java
@@ -0,0 +1,16 @@
+package com.battcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Levin
+ */
+@SpringBootApplication
+public class Chapter3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter3Application.class, args);
+    }
+
+}
---
+++ chapter3/src/main/java/com/battcn/controller/ThymeleafController.java
@@ -0,0 +1,79 @@
+package com.battcn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Levin
+ * @since 2018/4/23 0023
+ */
+@Controller
+@RequestMapping
+public class ThymeleafController {
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        ModelAndView view = new ModelAndView();
+        // 设置跳转的视图 默认映射到 src/main/resources/templates/{viewName}.html
+        view.setViewName("index");
+        // 设置属性
+        view.addObject("title", "我的第一个WEB页面");
+        view.addObject("desc", "欢迎进入battcn-web 系统");
+        Author author = new Author();
+        author.setAge(22);
+        author.setEmail("1837307557@qq.com");
+        author.setName("唐亚峰");
+        view.addObject("author", author);
+        return view;
+    }
+
+    @GetMapping("/index1")
+    public String index1(HttpServletRequest request) {
+        // TODO 与上面的写法不同，但是结果一致。
+        // 设置属性
+        request.setAttribute("title", "我的第一个WEB页面");
+        request.setAttribute("desc", "欢迎进入battcn-web 系统");
+        Author author = new Author();
+        author.setAge(22);
+        author.setEmail("1837307557@qq.com");
+        author.setName("唐亚峰");
+        request.setAttribute("author", author);
+        // 返回的 index 默认映射到 src/main/resources/templates/xxxx.html
+        return "index";
+    }
+
+    class Author {
+        private int age;
+        private String name;
+        private String email;
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
+}
