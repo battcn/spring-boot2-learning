cd5392bb5f381a15ed072343194ce1f41ef5a0db
Levin <1837307557@qq.com>
2018-05-10 10:20:31 +0800

    第九篇：整合Lettuce Redis

--- chapter8/src/main/java/com/battcn/config/RedisCacheAutoConfiguration.java
+++ chapter8/src/main/java/com/battcn/config/RedisCacheAutoConfiguration.java
@@ -4,9 +4,6 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -22,7 +19,7 @@ import java.io.Serializable;
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisCacheAutoConfiguration {
 
-    /*@Bean
+    @Bean
     public RedisTemplate<String, Serializable> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Serializable> template = new RedisTemplate<>();
         template.setKeySerializer(new StringRedisSerializer());
@@ -31,5 +28,5 @@ public class RedisCacheAutoConfiguration {
         redisConnectionFactory.setDatabase(5);
         template.setConnectionFactory(redisConnectionFactory);
         return template;
-    }*/
+    }
 }
