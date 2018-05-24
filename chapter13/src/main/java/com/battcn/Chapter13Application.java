531d759bc1461cc8443cb51d80a315002e4b9edf
Levin <1837307557@qq.com>
2018-05-24 15:54:49 +0800

    第十四篇：服务监控与管理

--- chapter13/src/main/java/com/battcn/Chapter13Application.java
+++ chapter13/src/main/java/com/battcn/Chapter13Application.java
@@ -1,60 +1,17 @@
 package com.battcn;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 
 /**
  * @author Levin
  */
 @SpringBootApplication
-@EnableAdminServer
 public class Chapter13Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter13Application.class, args);
     }
 
-    @Profile("dev")
-    @Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()
-                    .and().csrf().disable();
-        }
-    }
-
-    @Profile("prod")
-    @Configuration
-    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
-        private final String adminContextPath;
-
-        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
-            this.adminContextPath = adminServerProperties.getContextPath();
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-            successHandler.setTargetUrlParameter("redirectTo");
-
-            http.authorizeRequests()
-                    .antMatchers(adminContextPath + "/assets/**").permitAll()
-                    .antMatchers(adminContextPath + "/login").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
-                    .logout().logoutUrl(adminContextPath + "/logout").and()
-                    .httpBasic().and()
-                    .csrf().disable();
-        }
-    }
 }
