c0c645af1f5e712cbb497f4eea91f1471117645f
Levin <1837307557@qq.com>
2018-05-29 17:39:20 +0800

    第十六篇：定时任务

--- chapter15/src/main/java/com/battcn/task/SpringTaskDemo.java
+++ chapter15/src/main/java/com/battcn/task/SpringTaskDemo.java
@@ -23,19 +23,19 @@ public class SpringTaskDemo {
     @Scheduled(cron = "0/1 * * * * *")
     public void scheduled1() throws InterruptedException {
         Thread.sleep(3000);
-        log.info("每1秒执行一次：{}", LocalDateTime.now());
+        log.info("scheduled1 每1秒执行一次：{}", LocalDateTime.now());
     }
 
     @Scheduled(fixedRate = 1000)
     public void scheduled2() throws InterruptedException {
         Thread.sleep(3000);
-        log.info("每1秒执行一次：{}", LocalDateTime.now());
+        log.info("scheduled2 每1秒执行一次：{}", LocalDateTime.now());
     }
 
     @Scheduled(fixedDelay = 3000)
     public void scheduled3() throws InterruptedException {
         Thread.sleep(5000);
-        log.info("上次执行完毕后隔3秒继续执行：{}", LocalDateTime.now());
+        log.info("scheduled3 上次执行完毕后隔3秒继续执行：{}", LocalDateTime.now());
     }
 
 }
