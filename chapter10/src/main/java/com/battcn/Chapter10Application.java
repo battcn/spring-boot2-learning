75329e9c6a82963f33eaabca357fda98120fcb1f
Levin <1837307557@qq.com>
2018-06-01 17:24:07 +0800

    第十一篇：集成Swagger在线调试

--- chapter10/src/main/java/com/battcn/Chapter10Application.java
+++ chapter10/src/main/java/com/battcn/Chapter10Application.java
@@ -1,11 +1,13 @@
 package com.battcn;
 
+import com.battcn.swagger.annotation.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Levin
  */
+@EnableSwagger2Doc
 @SpringBootApplication
 public class Chapter10Application {
 
