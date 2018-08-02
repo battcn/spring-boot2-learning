b7167736f951f67bf2ad5ce2b8699718c60c02c2
Levin <1837307557@qq.com>
2018-08-02 16:22:51 +0800

    第二十七篇：优雅解决分布式限流

---
+++ chapter27/src/main/java/com/battcn/Chapter27Application.java
@@ -0,0 +1,17 @@
+package com.battcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Levin
+ */
+@SpringBootApplication
+public class Chapter27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter27Application.class, args);
+    }
+
+
+}
